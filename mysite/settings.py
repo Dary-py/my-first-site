1211d82e6fa049acf76bef1a7db40d4b1003b042
Dary-py <40460199+Dary-py@users.noreply.github.com>
2018-06-21 18:18:42 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,4 +125,3 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/user_uploads/'
 MEDIA_ROOT = BASE_DIR + '/uploads/'
 
-django_heroku.settings(locals())
